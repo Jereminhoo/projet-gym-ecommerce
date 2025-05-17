17356cb43ba738ea5a54e99bba3486956f3cd646
Jeremy <jeremy.muanza@condorcet.be>
2025-05-17 14:50:39 +0200

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_salle_de_gym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
