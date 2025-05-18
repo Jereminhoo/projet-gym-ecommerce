896d50eae2f3ff5a806b22e672c16c15c818f655
Mathieu-c360 <Mathieu.Colle@Condorcet.be>
2025-05-18 15:10:51 +0200

    ajout2

---
+++ Models/Utilisateur.cs
@@ -0,0 +1,12 @@
+﻿namespace Projet_salle_de_gym.Models
+{
+    public class Utilisateur
+    {
+        public int Id_utilisateur { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string mdp { get; set; }
+        public string mail { get; set; }
+        public bool admin { get; set; }
+    }
+}
