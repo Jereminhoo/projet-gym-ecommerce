2c1d6fc322150e34a818d8f123e7242283945a4e
Jeremy <jeremy.muanza@condorcet.be>
2025-05-18 15:47:42 +0200

    Ajout inscription

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,44 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Projet_salle_de_gym.Core.Infrastructure;
+using Projet_salle_de_gym.Models.Auth;
+
+namespace Projet_salle_de_gym.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IDbConnectionProvider _connectionProvider;
+
+        public AuthController(IDbConnectionProvider connectionProvider)
+        {
+            _connectionProvider = connectionProvider;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(UtilisateurRegisterModel model)
+        {
+            using var connection = await _connectionProvider.CreateConnection();
+
+            var hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Mdp);
+
+            var query = @"INSERT INTO utilisateur (nom_util, prenom_util, mail, mdp, admin)
+                          VALUES (@Nom_util, @Prenom_util, @Mail, @Mdp, false)";
+
+            await connection.ExecuteAsync(query, new
+            {
+                model.Nom_util,
+                model.Prenom_util,
+                model.Mail,
+                Mdp = hashedPassword
+            });
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Models/Auth/UtilisateurRegisterModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Projet_salle_de_gym.Models.Auth
+{
+    public class UtilisateurRegisterModel
+    {
+        public string Nom_util { get; set; }
+        public string Prenom_util { get; set; }
+        public string Mail { get; set; }
+        public string Mdp { get; set; }
+    }
+}
