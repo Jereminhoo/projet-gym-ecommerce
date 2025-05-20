931eb0d7d8e9c5c3bc69d7dc8a861b8ff40b2f2c
Mathieu-c360 <Mathieu.Colle@Condorcet.be>
2025-05-20 13:32:02 +0200

    affichage tous les users

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,12 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Projet_salle_de_gym.Core.Infrastructure;
+using Projet_salle_de_gym.Models.Utilisateurs;
 
 namespace Projet_salle_de_gym.Controllers
 {
     public class AdminController : Controller
     {
-        public IActionResult Index()
+        private readonly IDbConnectionProvider _connectionProvider;
+
+        public AdminController(IDbConnectionProvider connectionProvider)
         {
-            return View();
+            _connectionProvider = connectionProvider;
         }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            using var connection = await _connectionProvider.CreateConnection();
+
+
+            var utilisateurs = (await connection.QueryAsync<Utilisateur>(
+            "SELECT * FROM utilisateur WHERE admin = false"));
+
+
+            return View(utilisateurs.ToList()); 
+        }
+        
     }
 }
